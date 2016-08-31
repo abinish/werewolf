c0a6725df179c29ed2af417c5f91b0f9e4348f2c
abinish <austin.j.binish@gmail.com>
2016-08-31 18:51:28 -0500

    Fixing files

--- Werewolf.DAL/Werewolf.Web/Startup.cs
+++ Werewolf.DAL/Werewolf.Web/Startup.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.Owin;
 using Owin;
+[assembly: OwinStartup(typeof(Werewolf.Web.Startup))]
 
 namespace Werewolf.Web
 {
-	[assembly: OwinStartup(typeof(Werewolf.Web.Startup))]
+	
 	public class Startup
 	{
 		public void Configuration(IAppBuilder app)
